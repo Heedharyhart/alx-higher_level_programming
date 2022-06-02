8215b98af344e1a4c8c2b9bb28fd56f1ae275199
Idayat Abdulkareem <idayatabdulkareem@Administrators-MacBook-Pro-7.local>
2022-06-02 17:18:23 +0100

    0-Postive

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+import random
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(f"{}is positive")
+elif number == 0:
+    print(f"{number} is Zero".format(number))
+else:
+    print(f"{number} is negative".format(number))
